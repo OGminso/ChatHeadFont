c04a179fa38a059ca4da0b590bc1d800324b7252
Li2424 <lw-app-produktion@gmx.net>
2024-04-10 00:26:56 +0200

    Add support for player chat & death messages

--- src/main/java/net/minso/chathead/listener/PlayerListener.java
+++ src/main/java/net/minso/chathead/listener/PlayerListener.java
@@ -1,33 +1,48 @@
 package net.minso.chathead.listener;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.minso.chathead.API.ChatHeadAPI;
 import net.minso.chathead.API.SkinSource;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        ChatHeadAPI api = ChatHeadAPI.getInstance();
-
-        String joinMsg =
-                api.getHeadAsString(player, true, SkinSource.CRAFATAR) + " " + event.getJoinMessage();
-
-        event.setJoinMessage(joinMsg);
+        event.setJoinMessage(insertPlayerHead(event.getJoinMessage(), event.getPlayer()));
     }
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
+        event.setQuitMessage(insertPlayerHead(event.getQuitMessage(), event.getPlayer()));
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        event.setFormat(insertPlayerHead(event.getFormat(), event.getPlayer()));
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        event.setDeathMessage(insertPlayerHead(event.getDeathMessage(), event.getEntity()));
+    }
+
+    private String insertPlayerHead(String message, Player player) {
         ChatHeadAPI api = ChatHeadAPI.getInstance();
+        BaseComponent[] head = api.getHead(player, true, SkinSource.CRAFATAR);
+        BaseComponent[] msg = new ComponentBuilder()
+                .append(head)
+                .append(" ")
+                .append(message, ComponentBuilder.FormatRetention.NONE)
+                .create();
 
-        String quitMsg =
-                api.getHeadAsString(player, true, SkinSource.CRAFATAR) + " " + event.getQuitMessage();
-
-        event.setQuitMessage(quitMsg);
+        return TextComponent.toLegacyText(msg);
     }
 }
